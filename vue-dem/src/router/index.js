b766e3edb2acc4f7dc6897ea0958ef8e2eff835d
unknown <YIN KANGWEI>
2020-01-06 17:00:08 +0800

    list

--- vue-dem/src/router/index.js
+++ vue-dem/src/router/index.js
@@ -20,9 +20,6 @@ const routes = [
   },{
     path: '/list',
     name: 'list',
-    // route level code-splitting
-    // this generates a separate chunk (list.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/list.vue')
   }
 ]
--- vue-dem/src/views/list.vue
+++ vue-dem/src/views/list.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
-      列表
-  </div>
+  <div>列表页面</div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {}
+  },
+  methods: {}
 }
 </script>
 
 <style>
-
 </style>
