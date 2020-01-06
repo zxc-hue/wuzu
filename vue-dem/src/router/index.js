9aca147ac4b8011b4f65f287eecdd4b97d1817ff
unknown <YIN KANGWEI>
2020-01-06 16:11:52 +0800

    list

--- vue-dem/src/router/index.js
+++ vue-dem/src/router/index.js
@@ -17,6 +17,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },{
+    path: '/list',
+    name: 'list',
+    // route level code-splitting
+    // this generates a separate chunk (list.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/list.vue')
   }
 ]
 
